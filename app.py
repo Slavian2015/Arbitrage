f62e342209836a77140a5c6932214c5f762195e7
Slava <slavaku2014@gmail.com>
2020-04-06 16:34:13 +0300

    Start

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import flask
 
 external_stylesheets = [dbc.themes.SOLAR]
 app = flask.Flask(__name__)
-dash_app = dash.Dash(__name__,server=app,url_base_pathname="/",external_stylesheets=external_stylesheets)
+dash_app = dash.Dash(__name__,url_base_pathname="/",external_stylesheets=external_stylesheets)
+server=app
 # dash_app.layout = serve_layout
 dash_db = DashDatabase()  # create a DashDatabase instance for managing user values
 
