62442c74ef32a15ed687a5f79091b6730a2b4415
Slava <slavaku2014@gmail.com>
2020-04-10 16:34:57 +0300

    Three Parsers (beta)

--- VILKA.py
+++ VILKA.py
@@ -1,13 +1,5 @@
-import time
-import os.path
-import pandas
 import pandas as pd
-import os
-import csv
-import datetime as dt
-from dash import Dash, callback_context, no_update
-from dash.dependencies import Input, Output, State
-from dash_table import DataTable
+
 
 ##################################   SHOW ALL ROWS & COLS   ####################################
 pd.set_option('display.max_columns', None)
@@ -17,22 +9,22 @@ pd.set_option('max_colwidth', -1)
 
 alfa = {
     "USDBTC": {
-        "ask": [["7400"],["7395"],["7390"]],
-        "bid": [["7401"],["7405"],["7410"]],
+        "asks": [["7400"],["7395"],["7390"]],
+        "bids": [["7401"],["7405"],["7410"]],
     }
 }
 
 live = {
     "USDBTC": {
-        "ask": [["7420"],["7395"],["7390"]],
-        "bid": [["7430"],["7435"],["7440"]],
+        "asks": [["7420"],["7395"],["7390"]],
+        "bids": [["7430"],["7435"],["7440"]],
     }
 }
 
 hot = {
     "USDBTC": {
-        "ask": [["7500"],["7495"],["7400"]],
-        "bid": [["7410"],["7415"],["7460"]],
+        "asks": [["7500"],["7495"],["7400"]],
+        "bids": [["7410"],["7415"],["7460"]],
     }
 }
 
@@ -51,12 +43,12 @@ def tab(item, value):
         birga.append(value)
         valin.append(k[:3])
         valout.append(k[-3:])
-        rates.append(item[k]['bid'][0][0])
+        rates.append(item[k]['bids'][0][0])
 
         birga.append(value)
         valin.append(k[-3:])
         valout.append(k[:3])
-        rates.append(item[k]['ask'][0][0])
+        rates.append(item[k]['asks'][0][0])
     return
 
 for value, item in birgi.items():
